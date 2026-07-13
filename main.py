cab2ec75f6296630b62e3114ff81554b320e84fd
jsi3r3k <sierekjulia@gmail.com>
2026-07-13 20:59:18 +0200

    feat: add command line arguments

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 from detectors.rule_based import detect_with_rules
 from detectors.openai_detector import detect_with_openai
 from reports.console_report import print_console_report
@@ -23,24 +25,47 @@ def detect_prompt_attack(prompt, method):
         }
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Detect all possible attacks in prompt."
+    )
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        help="Prompt text to analyze."
+    )
+    parser.add_argument(
+        "--method",
+        type=str,
+        choices=["rules", "openai"],
+        default="rules",
+        help="Choice method of investigate your prompt."
+    )
+    parser.add_argument(
+        "--format",
+        type=str,
+        choices=["console", "json"],
+        default="console",
+        help="Choice your preffered raport format."
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    prompt = input("Prompt: ")
-    method = input("Detection method [rules/openai]: ")
-    report_format = input("Report format [console/json]: ")
+    args = parse_arguments()
 
-    if method == "":
-        method = "rules"
+    prompt = args.prompt
 
-    if report_format == "":
-        method = "console"
+    if prompt is None or prompt.strip == "":
+        prompt = input("Prompt: ")
 
-    result = detect_prompt_attack(prompt, method)
+    result = detect_prompt_attack(prompt, args.method)
 
-    if report_format == "json":
+    if args.format == "json":
         print_json_report(result)
-    elif report_format == "console":
+    else:
         print_console_report(result)
-
 
 if __name__ == "__main__":
     main()
